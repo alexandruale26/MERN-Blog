6ea53e8b49204dd4f22a3aa70224f632ff89e86e
Alexandru Tenie <alexandru_ale26@yahoo.com>
2024-06-01 20:21:54 +0300

    added error handler middleware

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,11 +1,12 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password || username === "" || email === "" || password === "") {
-    return res.status(400).json({ message: "All fields are required" });
+    return next(errorHandler(400, "All fields are required"));
   }
 
   try {
@@ -19,9 +20,6 @@ export const signup = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(500).json({
-      status: "fail",
-      message: error.message,
-    });
+    next(error);
   }
 };
