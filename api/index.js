55b58945ec407d50ed700ca12a4103b391121ae1
Alexandru Tenie <alexandru_ale26@yahoo.com>
2024-06-01 18:43:34 +0300

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uri = process.env.MONGOBD_URI.replace("<PASSWORD>", process.env.DB_PASSWORD);
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
