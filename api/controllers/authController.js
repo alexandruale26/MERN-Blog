98633e8ccd1f02db58cdcbba7348007535c56113
Alexandru Tenie <alexandru_ale26@yahoo.com>
2024-06-02 18:04:34 +0300

    created sign in api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
@@ -19,6 +20,33 @@ export const signup = async (req, res, next) => {
         user: newUser,
       },
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid credentials"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+    const { password: pass, ...rest } = validUser._doc;
+
+    res.status(200).cookie("access_token", token, { httpOnly: true }).json(rest);
   } catch (error) {
     next(error);
   }
